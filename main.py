470a5a8c0a29db87e9c4a60f47ff2e0679cd9809
mehrad <mehrad.shirvani.471@gmail.com>
2026-06-07 13:30:01 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# محاسبه مساحت و محیط مستطیل
+
+tool = float(input("طول مستطیل را وارد کنید: "))
+arz = float(input("عرض مستطیل را وارد کنید: "))
+
+masahat = tool * arz
+mohit = 2 * (tool + arz)
+
+print("مساحت مستطیل:", masahat)
+print("محیط مستطیل:", mohit)
